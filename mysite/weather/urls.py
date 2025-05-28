75d812e90ac959768bb5ba68022b178f2f7808db
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-05-28 16:37:03 +0500

    added history cities, and links for each

--- mysite/weather/urls.py
+++ mysite/weather/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import city_form, get_weather_for_city
+from .views import city_form, get_weather_for_city, get_weather_from_history
 
 app_name = 'weather'
 
 urlpatterns = [
     path("city/", city_form, name='search_city'),
-    path("result/", get_weather_for_city, name='result')
+    path("result/", get_weather_for_city, name='result'),
+    path("history-result/<int:index>/", get_weather_from_history, name="history_result"),
 ]
--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,10 +1,11 @@
 import json
 
 import pandas as pd
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.core.exceptions import BadRequest
+from requests import session
 
 from .forms import SearchCity
 from .services import get_location_coordinates, get_weather_by_latitude_and_longitude
@@ -12,6 +13,7 @@ from .services import get_location_coordinates, get_weather_by_latitude_and_long
 
 def city_form(request: HttpRequest) -> HttpResponse:
     """Получение локации"""
+    # request.session.delete()
     if request.method == "POST":
         form = SearchCity(request.POST)
         if form.is_valid():
@@ -20,11 +22,6 @@ def city_form(request: HttpRequest) -> HttpResponse:
                 search_history = request.session["search_history"]
             else:
                 search_history = []
-
-            search_history.append(location)
-            if len(search_history) > 10:
-                search_history = search_history[-10:]
-            request.session['search_history'] = search_history
 
             coordinates = get_location_coordinates(location=location)
             if coordinates:
@@ -37,26 +34,54 @@ def city_form(request: HttpRequest) -> HttpResponse:
                 hourly_table_list = hourly_table.to_dict(orient="records")
                 request.session['weather_data'] = weather_data
                 request.session['hourly'] = hourly_table_list
-                request.session['city_name'] = location
+                request.session['location'] = location
+                search_history.append({
+                    "weather_data": weather_data,
+                    "hourly": hourly_table_list,
+                    "location": location,
+                })
+                if len(search_history) > 10:
+                    search_history = search_history[-10:]
+                request.session['search_history'] = search_history
             else:
                 raise BadRequest(f"Location {location} is not find")
             return redirect(reverse("weather:result"))
     else:
         form = SearchCity()
         search_history = request.session.get('search_history')
-
     return render(request, 'weather/city-index.html',
                   context={"form": form, "search_history": search_history})
 
 
-def get_weather_for_city(request: HttpRequest, *args) -> HttpResponse:
+def get_weather_for_city(request: HttpRequest) -> HttpResponse:
     """Получение погоды для введённой локации"""
     weather_data = request.session.get("weather_data")
     hourly = request.session.get("hourly")
-    city_name = request.session.get("city_name")
+    location = request.session.get("location")
     context = {
         "weather_data": weather_data,
         "hourly": hourly,
-        "city_name": city_name,
+        "location": location,
     }
-    return render(request, "weather/result.html", context)
+    return render(request, "weather/result.html", context=context)
+
+
+def get_weather_from_history(request: HttpRequest, index: int) -> HttpResponse:
+    """
+    Достаёт прогноз погоды из истории поиска
+    :param request: HttpRequest
+    :param index: int - индекс локации из request.session["search_history]
+    :return: HttpResponse
+    """
+    search_history = request.session.get("search_history", [])
+    if index < 0 or index >= len(search_history):
+        raise Http404("Элемент истории не найден")
+    location = search_history[index]
+    context = {
+        "weather_data": location["weather_data"],
+        "hourly": location["hourly"],
+        "location": location["location"],
+    }
+
+    return render(request, "weather/result.html", context=context)
+
