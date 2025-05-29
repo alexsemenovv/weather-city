405d5a58bc1fe071a94f5c3cfe579e7fcd4d2888
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-05-29 05:50:19 +0500

    added model City

--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    """Город"""
+    name = models.CharField(verbose_name="Название города", unique=True, max_length=150)
+    count = models.IntegerField(verbose_name="Сколько раз искали этот город", blank=True, null=False, default=1)
+
+    def __str__(self):
+        return self.name
