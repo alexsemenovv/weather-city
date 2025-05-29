f48e9d73e050aceaa4d895a7da084e187d00cb13
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-05-29 13:03:47 +0500

    fix bugs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,11 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Путь к вашей папке static в корне
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
